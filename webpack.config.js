c040bde5991f64a23ee79364e804e75699135247
Adam Kamboj <kamboj.adam@gmail.com>
2021-03-05 23:10:17 -0500

    webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: '.app/javascript/channels/index.js',
+  entry: './app/javascript/channels/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
