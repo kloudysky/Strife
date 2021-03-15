1d69358b14dc0e0fe9edca9dc18108fde56c26cc
Adam Kamboj <kamboj.adam@gmail.com>
2021-03-15 17:18:13 -0400

    :sparkles: created store for redux

--- frontend/strife.jsx
+++ frontend/strife.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as api from './util/session_api_util'
+import configureStore from "./store/store"
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
@@ -9,3 +10,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
 window.login = api.login
 window.signup = api.signup
+
+const store = configureStore();
+// we don't put the store directly on the window because
+// it can be confusing when debugging, sometimes giving you access to state
+// when you shouldn't
+window.getState = store.getState;
+window.dispatch = store.dispatch; // just for testing!
