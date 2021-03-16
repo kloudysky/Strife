ce6523e45f553b685707292791c08a379491c7f6
Adam Kamboj <kamboj.adam@gmail.com>
2021-03-16 17:42:48 -0400

    :shirt:

--- frontend/strife.jsx
+++ frontend/strife.jsx
@@ -5,15 +5,28 @@ import * as api from './util/session_api_util'
 import configureStore from "./store/store"
 
 document.addEventListener("DOMContentLoaded", () => {
-  const store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+  store = configureStore();
+}
+  // const store = configureStore();
   // TESTING START
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
   // TESTING END
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, root);
 });
 
-window.login = api.login
-window.signup = api.signup
-window.logout = api.logout
+// window.login = api.login
+// window.signup = api.signup
+// window.logout = api.logout
