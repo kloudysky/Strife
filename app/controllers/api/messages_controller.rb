9ab5cb0a45ed1e9c7a85d6c69fbcd933db805679
Adam Kamboj <kamboj.adam@gmail.com>
2021-03-18 17:04:17 -0400

    :bug:

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,50 +1,50 @@
 class Api::MessagesController < ApplicationController
-    before_action :require_logged_in
+  before_action :require_logged_in
 
-    def index
-        @messages = Channel.find_by(id: params[:id]).messages
-        render :index
+  def index
+    @messages = Channel.find_by(id: params[:id]).messages
+    render :index
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      render :show
+    else
+      render json: @message.errors.full_messages, status: 422
     end
+  end
 
-    def create 
-        @message = Message.new(message_params)
+  def show
+    @message = Message.find(params[:id])
+    render :show
+  end
 
-        if @message.save
-            render :show
-        else
-            render json @message.errors.full_messages, status: 422
-        end
+  def edit
+    @message = Message.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @message = Message.find(params[:id])
+
+    if @message.update(message_params)
+      #render :show_exceptions
+    else
+      render json: @message.errors.full_messages, status: 422
     end
+  end
 
-    def show
-        @message = Message.find(params[:id])
-        render :show
-    end
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+    # render :index
+  end
 
-    def edit
-        @message = Message.find(params[:id])
-        render :edit
-    end
+  private
 
-    def update
-        @message = Message.find(params[:id])
-
-        if @message.update(message_params)
-            #render :show_exceptions
-        else
-            render :json @message.errors.full_messages, status: 422
-        end
-    end
-
-    def destroy
-        @message = Message.find(params[:id])
-        @message.destroy
-        # render :index
-    end
-
-    private
-
-    def message_params
-        params.require(:message).permit(:author_id, :channel_id, :content)
-    end
+  def message_params
+    params.require(:message).permit(:author_id, :channel_id, :content)
+  end
 end
