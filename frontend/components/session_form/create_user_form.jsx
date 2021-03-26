66a2cf1dfb54fa1bd630e992357d1533ce22b0bd
Adam Kamboj <kamboj.adam@gmail.com>
2021-03-26 10:46:10 -0400

    :lipstick:

--- frontend/components/session_form/create_user_form.jsx
+++ frontend/components/session_form/create_user_form.jsx
@@ -39,60 +39,62 @@ class CreateUserForm extends React.Component {
           <a href="/">STRIFE</a>
         </div>
         <div className="login-form-box fade-in">
-          <h2 className="login-header">Create an account</h2>
-          <div className="login-form-box">
-            <form onSubmit={this.handleSubmit} className="login-form form">
-              {this.renderErrors()}
-              <div className="form-field">
-                <label>Email</label>
-                <input
-                  type="email"
-                  value={this.state.email}
-                  onChange={this.update("email")}
-                  className="login-input"
-                />
-              </div>
-              <br />
-              <div className="form-field">
-                <label>Username</label>
-                <input
-                  type="text"
-                  value={this.state.username}
-                  onChange={this.update("username")}
-                  className="login-input"
-                />
-              </div>
-              <br />
-              <div className="form-field">
-                <label>Password</label>
-                <input
-                  type="password"
-                  value={this.state.password}
-                  onChange={this.update("password")}
-                  className="login-input"
-                />
-              </div>
-              <br />
-              <div className="form-field">
-                <label>Date of Birth</label>
-                <input
-                  type="date"
-                  max="2018-12-31"
-                  value={this.state.birthday}
-                  onChange={this.update("birthday")}
-                  className="login-input"
-                />
-              </div>
-              <br />
-              <div className="form-field">
-                <button type="submit" className="form-login-button">
-                  Continue
-                </button>
-              </div>
-            </form>
+          <div className="sign-up-form-container">
+            <h2 className="login-header">Create an account</h2>
+            <div className="login-form-box">
+              <form onSubmit={this.handleSubmit} className="login-form form">
+                {this.renderErrors()}
+                <div className="form-field">
+                  <label>Email</label>
+                  <input
+                    type="email"
+                    value={this.state.email}
+                    onChange={this.update("email")}
+                    className="login-input"
+                  />
+                </div>
+                <br />
+                <div className="form-field">
+                  <label>Username</label>
+                  <input
+                    type="text"
+                    value={this.state.username}
+                    onChange={this.update("username")}
+                    className="login-input"
+                  />
+                </div>
+                <br />
+                <div className="form-field">
+                  <label>Password</label>
+                  <input
+                    type="password"
+                    value={this.state.password}
+                    onChange={this.update("password")}
+                    className="login-input"
+                  />
+                </div>
+                <br />
+                <div className="form-field">
+                  <label>Date of Birth</label>
+                  <input
+                    type="date"
+                    max="2018-12-31"
+                    value={this.state.birthday}
+                    onChange={this.update("birthday")}
+                    className="login-input"
+                  />
+                </div>
+                <br />
+                <div className="form-field">
+                  <button type="submit" className="form-login-button">
+                    Continue
+                  </button>
+                </div>
+              </form>
+            </div>
+            <br />
+            {this.props.navLink}
           </div>
-          <br />
-          {this.props.navLink}
         </div>
       </div>
     );
