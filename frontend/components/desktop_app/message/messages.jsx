546f7b9f01532dbc21cc17cd13afd959a349060e
Adam Kamboj <kamboj.adam@gmail.com>
2021-06-21 13:40:51 -0400

    fixed concat msgs within 1 minute

--- frontend/components/desktop_app/message/messages.jsx
+++ frontend/components/desktop_app/message/messages.jsx
@@ -124,30 +124,46 @@ class Messages extends React.Component {
   }
 
   timeSince(date) {
+    if (typeof date !== "object") {
+      date = new Date(date);
+    }
+
     var seconds = Math.floor((new Date() - date) / 1000);
+    var intervalType;
 
-    var interval = seconds / 31536000;
+    var interval = Math.floor(seconds / 31536000);
+    if (interval >= 1) {
+      intervalType = "year";
+    } else {
+      interval = Math.floor(seconds / 2592000);
+      if (interval >= 1) {
+        intervalType = "month";
+      } else {
+        interval = Math.floor(seconds / 86400);
+        if (interval >= 1) {
+          intervalType = "day";
+        } else {
+          interval = Math.floor(seconds / 3600);
+          if (interval >= 1) {
+            intervalType = "hour";
+          } else {
+            interval = Math.floor(seconds / 60);
+            if (interval >= 1) {
+              intervalType = "minute";
+            } else {
+              interval = seconds;
+              intervalType = "second";
+            }
+          }
+        }
+      }
+    }
 
-    if (interval > 1) {
-      return Math.floor(interval) + " years";
+    if (interval > 1 || interval === 0) {
+      intervalType += "s";
     }
-    interval = seconds / 2592000;
-    if (interval > 1) {
-      return Math.floor(interval) + " months";
-    }
-    interval = seconds / 86400;
-    if (interval > 1) {
-      return Math.floor(interval) + " days";
-    }
-    interval = seconds / 3600;
-    if (interval > 1) {
-      return Math.floor(interval) + " hours";
-    }
-    interval = seconds / 60;
-    if (interval > 1) {
-      return Math.floor(interval) + " minutes";
-    }
-    return Math.floor(seconds) + " seconds";
+
+    return interval + " " + intervalType;
   }
 }
 
--- frontend/reducers/messages_reducer.js
+++ frontend/reducers/messages_reducer.js
@@ -6,10 +6,18 @@ export default (state = [], action) => {
     case actions.RECEIVE_CHANNEL_MESSAGES:
       const result = action.messages.reduce((r, o) => {
         const last = r[r.length - 1];
-
-        if (last && last.author_id === o.author_id)
-          last.content = last.content + `\n` + o.content;
-        else r.push({ ...o });
+        if (
+          last &&
+          Date.parse(o.created_at) - Date.parse(last.created_at) < 60000
+        ) {
+          if (last && last.author_id === o.author_id) {
+            last.content = last.content + `\n` + o.content;
+          } else {
+            r.push({ ...o });
+          }
+        } else {
+          r.push({ ...o });
+        }
 
         return r;
       }, []);
@@ -19,12 +27,22 @@ export default (state = [], action) => {
         state.length > 0 &&
         state.slice(-1)[0].author_id === action.message.author_id
       ) {
-        const newMsg = Object.assign({}, action.message, {
-          content: state.slice(-1)[0].content + "\n" + action.message.content,
-        });
-        let newState = state;
-        const nextState = newState.slice(0, -1);
-        return [...nextState, newMsg];
+        console.log(
+          Date.parse(action.message.created_at) -
+            Date.parse(state.slice(-1)[0].created_at)
+        );
+        if (
+          Date.parse(action.message.created_at) -
+            Date.parse(state.slice(-1)[0].created_at) <
+          60000
+        ) {
+          const newMsg = Object.assign({}, action.message, {
+            content: state.slice(-1)[0].content + "\n" + action.message.content,
+          });
+          let newState = state;
+          const nextState = newState.slice(0, -1);
+          return [...nextState, newMsg];
+        }
       }
       return [...state, action.message];
     default:
